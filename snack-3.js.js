2d486e5f6b42297b373626101531a501b5a18361
Tamburini-Christofer <christofer.tamburini92@gmail.com>
2025-12-15 09:58:55 +0100

    snack-3

---
+++ snack-3.js.js
@@ -0,0 +1,60 @@
+const books = [
+  { 
+	  title: "React Billionaire", 
+	  pages: 250, 
+	  author: {
+		  name: 'Alice',
+		  age: 35
+	  },
+	  available: false,
+	  price: '101€',
+	  tags: ['advanced', 'js', 'react', 'senior']
+  },
+  { 
+	  title: "Advanced JS", 
+	  pages: 500, 
+	  author: {
+		  name: 'Bob',
+		  age: 20
+	  },
+	  available: true,
+	  price: '25€',
+	  tags: ['advanced', 'js', 'mid-senior']
+  },
+  { 
+	  title: "CSS Secrets", 
+	  pages: 320, 
+	  author: {
+		  name: 'Alice',
+		  age: 17
+	  },
+	  available: true,
+	  price: '8€',
+	  tags: ['html', 'css', 'junior']
+  },
+  { 
+	  title: "HTML Mastery", 
+	  pages: 200, 
+	  author: {
+		  name: 'Charlie',
+		  age: 50
+	  },
+	  available: false,
+	  price: '48€',
+	  tags: ['html', 'advanced', 'junior', 'mid-senior']
+  },
+];
+
+const authors = books.map(b => b.author)
+console.log(authors)
+
+const areAuthorsAdults = authors.every(a => a.age >= 18)
+console.log(areAuthorsAdults)
+
+authors.sort((a, b) => {
+  return areAuthorsAdults
+    ? a.age - b.age
+    : b.age - a.age;
+});
+
+console.log(authors);
